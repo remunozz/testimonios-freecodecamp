77273b6c687da36287bc962f84499cc78df58f97
remunozz <remunozz@gmail.com>
2023-01-14 22:05:20 -0500

    primeros estilos

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,10 +3,15 @@ import React from 'react';
 function Testimonio (){
     return (
         <div className = 'contenedor-testimonio'>
-          <img 
-            className = 'imagen-testimonio'
-            src={require('../imagenes/testimonio-emma.png')} 
-            alt='Foto de Emma'/>
+            <img 
+                className = 'imagen-testimonio'
+                src={require('../imagenes/testimonio-emma.png')} 
+                alt='Foto de Emma'/>
+            <div className='contenedor-texto-testimonio'>
+                <p className = 'nombre-testimonio'>Emma Bostian en Suecia</p>
+                <p className = 'cargo-testimonio'>Ingeniera de Software en Spotify</p>
+                <p className = 'texo-testimonio'>"Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
+            </div> 
         </div>
     );
 }
